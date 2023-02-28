72e907af5cd6dab5b4baff3ec404524864f6bab9
Leonid <ewro01@yandex.ru>
2023-03-01 01:21:55 +0300

    test /send to project with rabbit mq

--- src/main/java/ru/leonid/ClientTelegramSpring/Controller/HomepageController.java
+++ src/main/java/ru/leonid/ClientTelegramSpring/Controller/HomepageController.java
@@ -2,15 +2,19 @@ package ru.leonid.ClientTelegramSpring.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import ru.leonid.ClientTelegramSpring.Service.BotService;
 import ru.leonid.ClientTelegramSpring.Service.BotServiceInterface;
+
+import java.net.Authenticator;
 
 
 @Controller
 public class HomepageController {
 
     final BotServiceInterface botService;
+    private Long offset = 5L;
     public HomepageController(BotService botService) {
         this.botService = botService;
     }
@@ -23,9 +27,25 @@ public class HomepageController {
 
     @GetMapping("/update")
     public String getUpdatesfrombot(Model model){
-        long offset = 5L;
+        model.addAttribute("offset", offset);
         model.addAttribute("updates", botService.getUpdates(offset));
         return "listofdata";
+    }
+
+    @PostMapping("/update")
+    public String sendUpdateToTelegram(@ModelAttribute Long offset, Model model){
+        System.out.println("получили оффсет: "+ offset);
+        return "listofdata";
+    }
+//сделать столбец юзернейм в представлении гиперссылкой с переходом на другую страницу для ответа пользователю.
+
+    //тест rest + rabbitmq project
+    @PostMapping("/send")
+    public String sendToRabbit(@RequestParam("mymessage") String message){
+        System.out.println("Отправляем: "+ message);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject("http://127.0.0.1:8080/send?msgtorabbit="+ message, String.class);
+        return "redirect:/update";
     }
 
 }
