12aeb87563ec76a16a0a7349e42a83b822d03b47
Leonid <ewro01@yandex.ru>
2023-03-07 15:14:33 +0300

    io exeption in send to rabbit proj

--- src/main/java/ru/leonid/ClientTelegramSpring/Controller/HomepageController.java
+++ src/main/java/ru/leonid/ClientTelegramSpring/Controller/HomepageController.java
@@ -1,8 +1,15 @@
 package ru.leonid.ClientTelegramSpring.Controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import ru.leonid.ClientTelegramSpring.Service.BotService;
 import ru.leonid.ClientTelegramSpring.Service.BotServiceInterface;
@@ -15,6 +22,7 @@ public class HomepageController {
 
     final BotServiceInterface botService;
     private Long offset = 5L;
+    private String sendStatus = "";
     public HomepageController(BotService botService) {
         this.botService = botService;
     }
@@ -28,6 +36,8 @@ public class HomepageController {
     @GetMapping("/update")
     public String getUpdatesfrombot(Model model){
         model.addAttribute("offset", offset);
+        model.addAttribute("sendstatus",sendStatus);
+        sendStatus = "";
         model.addAttribute("updates", botService.getUpdates(offset));
         return "listofdata";
     }
@@ -41,10 +51,16 @@ public class HomepageController {
 
     //тест rest + rabbitmq project
     @PostMapping("/send")
-    public String sendToRabbit(@RequestParam("mymessage") String message){
+    public String sendToRabbit(@RequestParam("mymessage") String message, Model model){
         System.out.println("Отправляем: "+ message);
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getForObject("http://127.0.0.1:8080/send?msgtorabbit="+ message, String.class);
+        try {
+            restTemplate.getForObject("http://127.0.0.1:8080/send?msgtorabbit=" + message, String.class);
+        }catch (RestClientException ioe){
+            sendStatus = "Ошибка отправки. Сервис недоступен.    [" + ioe.getMessage() + "]";
+            return "redirect:/update";
+        }
+        sendStatus = "Сообщение отправлено: [" + message + "]";
         return "redirect:/update";
     }
 
